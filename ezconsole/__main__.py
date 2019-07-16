f43bf5d522c4ae09f51732272784c0b1556c0631
Niels Boehm <blubberdiblub@gmail.com>
2019-07-16 06:21:16 +0200

    Adjust try-out code

--- ezconsole/__main__.py
+++ ezconsole/__main__.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
-from . import ez_dialog, Choice
+from . import ez_dialog, Choice, Console
 
 
 def _main():
     import time
 
-    print("Das Lamm sagt Hurz!")
+    print("Das Lamm sagt Hurz!", end='', flush=True)
 
     time.sleep(0.5)
 
-    ez_dialog(Choice(["foo", "bar", "foobar", "baz"]))
+    console = Console()
+
+    ez_dialog(Choice(["1", "wunschvorstellung", "3", "4", "fünf", "sechs"]), console=console)
+
+    time.sleep(1)
+    ez_dialog(Choice(["foo", "bar", "foobar", "baz"]), console=console)
+
+    time.sleep(1)
+    ez_dialog(Choice(["1", "wunschvorstellung", "3", "4", "fünf", "sechs", "sieben", "acht"]), console=console)
+
+    time.sleep(1)
+    ez_dialog(Choice(["hundert", "foo", "bar"]), console=console)
 
 
 _main()
